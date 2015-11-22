0afb9cd8fef3e4e15d20b703c1b48ce560bda372
mrpandat <mrpandat@hotmail.fr>
2015-11-22 17:51:47 +0100

    refactoring server.c for coding style

--- src/server.c
+++ src/server.c
@@ -1,49 +1,30 @@
 #include "server.h"
 
 
-void launchApp(uint16_t port) {
-
-
-
-
+void launchApp(uint16_t port)
+{
     int my_socket = socket(AF_INET, SOCK_STREAM, 0);
-
     if(my_socket == -1)
         perror("Create socket");
-
     struct sockaddr_in address;
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
-
     void* address_ptr = &address;
     struct sockaddr *sockaddr_f = address_ptr;
     if(bind(my_socket, sockaddr_f , sizeof(address)) < 0) {
-            perror("Error");
-            exit(-1);
-        }
-
-
-
-    if (listen(my_socket, 5) == -1) {
-        perror("listen()");
+        perror("Error");
+        exit(-1);
     }
-
+    if (listen(my_socket, 5) == -1)
+        printf(stderr, "Error while listening to the connection");
     struct sockaddr_in *csin = {0};
     void* csin_ptr = csin;
     struct sockaddr *sock_addr_connect = csin_ptr;
-
-    int caller;
-
     socklen_t sinsize = sizeof csin;
-
     printf("waiting for connection:\n");
-    caller = accept(my_socket, sock_addr_connect,  &sinsize);
-
-    if (caller == -1) {
-        perror("accept()");
-    }
-
+    int caller = accept(my_socket, sock_addr_connect,  &sinsize);
+    if (caller == -1)
+        printf(stderr, "Error while launching accept()");
 
 }
